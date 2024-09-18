1d3efedecd8ec22ce12298dfb42f712f18e02b56
Vamonosraza <martinezjessy393@gmail.com>
2024-09-17 20:22:09 -0700

    Greeting Test passed

--- main.py
+++ main.py
@@ -2,5 +2,8 @@
 def hello_world():
     return "Hello, Git World!"
 
+def greet_person(name):
+    return f"Hello, {name}!"
+
 if __name__ == "__main__":
     print(hello_world())
--- test_main.py
+++ test_main.py
@@ -1,10 +1,13 @@
 # test_main.py
 import unittest
-from main import hello_world
+from main import hello_world, greet_person
 
 class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello, Git World!")
+
+    def test_greet_person(self):
+        self.assertEqual(greet_person("Jessy"), "Hello, Jessy!")
 
 if __name__ == '__main__':
     unittest.main()
